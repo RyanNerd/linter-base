b8f52634bea19225030625a04117904ddac7eedc
ryan <ryan.jentzsch@gmail.com>
2022-01-14 07:54:36 -0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,200 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true
+    },
+    parser: '@typescript-eslint/parser',
+    root: true,
+    parserOptions: {
+        ecmaVersion: 13,
+        requireConfigFile: false,
+        ecmaFeatures: {
+            jsx: true
+        },
+        sourceType: 'module'
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {}
+        },
+        react: {
+            version: 'detect'
+        },
+        jsdoc: {
+            structuredTags: {
+                JSX: {type: true}
+            },
+            definedTags: ['link']
+        }
+    },
+    extends: [
+        'prettier',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended'
+    ],
+    plugins: [
+        'eslint-plugin-jsdoc',
+        'eslint-plugin-prefer-arrow',
+        'eslint-plugin-react',
+        'react',
+        'react-hooks',
+        '@typescript-eslint'
+    ],
+    rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'arrow-parens': ['off', 'always'],
+        'brace-style': ['off', 'off'],
+        'comma-dangle': 'error',
+        complexity: 'off',
+        'constructor-super': 'error',
+        'dot-notation': 'error',
+        'eol-last': 'off',
+        eqeqeq: ['error', 'smart'],
+        'guard-for-in': 'error',
+        'id-denylist': [
+            'error',
+            'any',
+            'Number',
+            'number',
+            'String',
+            'string',
+            'Boolean',
+            'boolean',
+            'Undefined',
+            'undefined'
+        ],
+        'id-match': 'error',
+        indent: 'off',
+        'max-classes-per-file': ['error', 1],
+        'linebreak-style': 'off',
+        'jsdoc/check-alignment': 'error',
+        'jsdoc/check-indentation': 'error',
+        'jsdoc/newline-after-description': 'off',
+        'jsdoc/no-undefined-types': 1,
+        'jsdoc/check-line-alignment': 1,
+        'jsdoc/check-param-names': 1,
+        'jsdoc/check-property-names': 1,
+        'jsdoc/check-syntax': 1,
+        'jsdoc/check-tag-names': ['error', {definedTags: ['link']}],
+        'jsdoc/check-types': 1,
+        'jsdoc/check-values': 1,
+        'jsdoc/empty-tags': 1,
+        'jsdoc/implements-on-classes': 1,
+        'jsdoc/multiline-blocks': 1,
+        'jsdoc/no-bad-blocks': 1,
+        'jsdoc/no-defaults': 1,
+        'jsdoc/no-multi-asterisks': 1,
+        'jsdoc/require-asterisk-prefix': 1,
+        'jsdoc/require-description': 1,
+        'jsdoc/require-jsdoc': 1,
+        'jsdoc/require-param': 1,
+        'jsdoc/require-param-description': 1,
+        'jsdoc/require-param-name': 1,
+        'jsdoc/require-param-type': 1,
+        'jsdoc/require-property': 1,
+        'jsdoc/require-property-description': 1,
+        'jsdoc/require-property-name': 1,
+        'jsdoc/require-property-type': 1,
+        'jsdoc/require-returns-check': 1,
+        'jsdoc/require-returns-description': 1,
+        'jsdoc/require-returns-type': 1,
+        'jsdoc/require-throws': 1,
+        'jsdoc/require-yields': 1,
+        'jsdoc/require-yields-check': 1,
+        'jsdoc/tag-lines': 1,
+        'jsdoc/valid-types': ['error', {allowEmptyNamepaths: true, checkSeesForNamepaths: false}],
+        'jsdoc/check-access': 1,
+        'jsdoc/check-examples': 1,
+        'jsdoc/match-description': 'off',
+        'jsdoc/no-missing-syntax': 'off',
+        'jsdoc/no-restricted-syntax': 'off',
+        'jsdoc/no-types': 'off',
+        'jsdoc/require-description-complete-sentence': 'off',
+        'jsdoc/require-example': 'off',
+        'jsdoc/require-file-overview': 'off',
+        'jsdoc/require-hyphen-before-param-description': 'off',
+        'jsdoc/require-returns': 'off',
+        'max-len': [
+            'error',
+            {
+                ignoreUrls: true,
+                code: 120
+            }
+        ],
+        'new-parens': 'off',
+        'newline-per-chained-call': 'off',
+        'no-array-constructor': 'off',
+        'no-bitwise': 'error',
+        'no-caller': 'error',
+        'no-cond-assign': 'error',
+        'no-console': 'off',
+        'no-debugger': 'error',
+        'no-empty': 'error',
+        'no-empty-function': 'error',
+        'no-eval': 'error',
+        'no-extra-semi': 'off',
+        'no-fallthrough': 'off',
+        'no-implied-eval': 'off',
+        'no-invalid-this': 'off',
+        'no-irregular-whitespace': 'error',
+        'no-multiple-empty-lines': 'error',
+        'no-new-wrappers': 'error',
+        'no-shadow': 'off',
+        'no-throw-literal': 'error',
+        'no-trailing-spaces': 'off',
+        'no-undef-init': 'error',
+        'no-underscore-dangle': 'off',
+        'no-unsafe-finally': 'error',
+        'no-unused-expressions': 'error',
+        'no-unused-labels': 'error',
+        'no-unused-vars': 'off',
+        'no-use-before-define': 'off',
+        'no-var': 'error',
+        'object-shorthand': 'error',
+        'one-var': ['error', 'never'],
+        'padded-blocks': [
+            'off',
+            {
+                blocks: 'never'
+            },
+            {
+                allowSingleLineBlocks: false
+            }
+        ],
+        'prefer-arrow/prefer-arrow-functions': 'error',
+        'prefer-const': 'error',
+        'quote-props': 'off',
+        quotes: 'off',
+        radix: 'off',
+        'react/jsx-curly-spacing': 'off',
+        'react/jsx-equals-spacing': 'off',
+        'react/jsx-tag-spacing': [
+            'off',
+            {
+                afterOpening: 'allow',
+                closingSlash: 'allow'
+            }
+        ],
+        'react/jsx-wrap-multilines': 'off',
+        'require-await': 'off',
+        'react-hooks/exhaustive-deps': 'error',
+        semi: 'off',
+        'space-before-function-paren': 'off',
+        'space-in-parens': ['off', 'never'],
+        'spaced-comment': [
+            'error',
+            'always',
+            {
+                markers: ['/']
+            }
+        ],
+        'use-isnan': 'error',
+        'valid-typeof': 'off'
+    },
+    globals: {
+        JSX: true
+    }
+};
+
